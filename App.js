7b9f168a21776ea5c99b23f63141e3c7d4917ee7
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2020-10-17 19:26:16 +0200

    Added line-through when done

--- App.js
+++ App.js
@@ -2,22 +2,36 @@ var app = new Vue({
     el: '#app',
     data: {
         title: '',
+        id: 0,
         todos: []
     },
     methods: {
         createToDo: function(title) {
             if (title != "") {
                 this.todos.push({
-                    id: 1,
+                    id: this.id,
                     title: title,
                     done: false
                 })
+                this.id = this.id + 1;
             }
         },
         deleteToDo: function(todo) {
             for (let i = 0; i < this.todos.length; i++) {
                 if (todo.id == this.todos[i].id) {
-                    this.todos.splice(i, 1);
+                    this.todos.splice(i, 1)
+                }
+            }
+        },
+        update: function(todo) {
+            let el = document.getElementById(todo.id)
+            el.style.textDecoration = "none"
+            for (let i = 0; i < this.todos.length; i++) {
+                if (todo.done) {
+                    return true
+                } else {
+                    el.style.textDecoration = "line-through"
+                    return false
                 }
             }
         }
